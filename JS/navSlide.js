4dd631a02eeedd1ffc218d8ed6e882a939f083f8
RobMc_11 <rob.mccormack@live.com>
2020-12-31 20:04:12 +0000

    progressflex

--- JS/navSlide.js
+++ JS/navSlide.js
@@ -1,9 +1,11 @@
 const navSlide = () => {
     const burger_menu = document.querySelector('.burger_menu');
-    const nav = document.querySelector('.nav_links1');
+    const nav1 = document.querySelector('.nav_links1');
+    const nav = document.querySelector('.nav_links');
 
     burger_menu.addEventListener('click',() => {
-        nav.classList.toggle('nav_active');
+        nav1.classList.toggle('nav_active');
+        nav.classList.toggle('nav_deactive');
     });
 }
 
