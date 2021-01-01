64674152cb226447f9b884e09c7d86be0eeabcd5
RobMc_11 <rob.mccormack@live.com>
2021-01-01 18:21:36 +0000

    Nav bar fix

--- JS/navSlide.js
+++ JS/navSlide.js
@@ -1,12 +1,16 @@
 const navSlide = () => {
-    const burger_menu = document.querySelector('.burger_menu');
-    const nav1 = document.querySelector('.nav_links1');
-    const nav = document.querySelector('.nav_links');
+  const burger_menu = document.querySelector(".burger_menu");
+  const nav1 = document.querySelector(".nav_links1");
+  const nav = document.querySelector(".nav_links");
 
-    burger_menu.addEventListener('click',() => {
-        nav1.classList.toggle('nav_active');
-        nav.classList.toggle('nav_deactive');
-    });
-}
+
+  burger_menu.addEventListener("click", () => {
+    nav1.classList.toggle("nav_active");
+    nav.classList.toggle("nav_deactive");
+
+    //Burger animation
+    burger_menu.classList.toggle("toggle");
+  });
+};
 
 navSlide();
